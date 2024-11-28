9523dac07004e7b0ebe757b55cfda1454f4d9e78
誠一郎 <sinada@trust-coms.com>
2024-11-28 11:30:16 +0900

    3kaime

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on thes 品田
       </p>
     </>
   )
