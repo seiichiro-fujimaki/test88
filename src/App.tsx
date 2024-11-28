6ceb5fd83b17f76056c34fd64753751362e04217
誠一郎 <sinada@trust-coms.com>
2024-11-28 16:07:41 +0900

    niigata4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
+import hason1 from './assets/hason.jpeg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -12,14 +12,11 @@ function App() {
         <a href="https://trust-coms.com/" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        {/* <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+        <a href="https://react.dev" target="_blank">
+          <img src={hason1} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
-
-
-
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
